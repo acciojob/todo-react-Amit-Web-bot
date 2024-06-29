8fff58fe7411d25d56c055c90dd0e0b40ee171b3
Amit <84635639+Amit-Web-bot@users.noreply.github.com>
2024-06-29 10:52:41 +0000

    "First Commit"

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[task,setTask]=useState("")
+  const [todoList, settodoList]= useState([])
+  function addTask(){
+    index = index+1
+    settodoList([{id: index, data: task},...todoList])
+    setTask("");
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <input placeholder="Enter the work" value={task} onChange={(e)=>{
+      setTask(e.target.value)
+        }}></input>
+        <button onClick={addTask}>Add the Task</button>
+        {todoList.map((task)=>{
+          return(<div>
+            <li key={task.id}>{task.data}</li>
+            <button onClick={()=>{
+            settodoList(todoList.filter(a=>a.id!==task.id))
+            }}>Delete</button>
+            </div>
+          )
+        })}
     </div>
   )
 }
